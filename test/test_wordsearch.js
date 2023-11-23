bae063626be2c6c1227eee1ba7673f6cadc5f9b6
saintsappho <robinrosefleur@gmail.com>
2023-11-22 19:03:29 -0700

    wordsearch and tests, Tandra and Bilqis as teammates!

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,7 +4,7 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the word is not present horizontally or vertically", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,35 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'K', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'R', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'M', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'E', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'R', 'N', 'E', 'A', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'Y', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'KRAMER')
+
+    assert.isTrue(result);
+  });
+  it("should return false if the word is not present horizontally or vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'Y', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'A', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ELAINE')
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  let newMatrix = []
+  for (let i = 0; i < letters.length; i++) {
+    let newArr = []
+    for (let x = 0; x < letters[i].length; x++){
+      newArr.push(letters[x][i]);
     }
-}
-
+    newMatrix.push(newArr)
+  }
+  const row1 = letters.map(items => items.join(''))
+  for (item of row1) {
+    if (item.includes(word)) return true
+  } 
+  const row2 = newMatrix.map(items => items.join(''))
+  for (item of row2) {
+    if (item.includes(word)) return true
+  } 
+  return false
+} 
 module.exports = wordSearch
